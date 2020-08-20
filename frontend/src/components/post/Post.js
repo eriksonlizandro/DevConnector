9662d05ae5cea4553a5ef4df19abfb1b8e8d7be9
eriksonlizandro <eegs.sist@gmail.com>
2020-08-20 03:53:48 -0600

    Almost Done

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
-import PostItem from './CommentItem';
+import PostItem from '../posts/PostItem';
 import CommentForm from '../post/CommentForm';
 import CommentItem from '../post/CommentItem';
 import { getPost } from '../../actions/post';
